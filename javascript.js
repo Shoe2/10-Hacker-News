8ff20fdfdac6b7078f9b80a535e2f9d599fd7d9d
shoeboxgames <shoe_wooberg@snobbywalrus.com>
2017-06-06 22:15:04 -0700

    Basic working mechanism to print out articles
    Articles are missing author karma score, and sometimes duplicates may
    appear if the random number generator chooses the same number more than
    once

--- javascript.js
+++ javascript.js
@@ -1,11 +1,49 @@
 var app = angular.module("storyApp", []);
 app.controller('storyAppController', ['$scope', '$http', function ($scope, $http) {
-    $scope.refreshStories = function () {
-        $http.get('https://hacker-news.firebaseio.com/v0/topstories.json').
-            then(function (response) {
-                $scope.stories = response.data;
-                console.log(response.data);
-            }, function (response) { console.log("ERROR:" + response.ExceptionMessage); });
-    };
+        
+    $http.get('https://hacker-news.firebaseio.com/v0/topstories.json').
+        then(function successCallback(response) {
+            $scope.data = response.data;
+            $scope.stories = [];
+            $scope.storyData = [];
+            
+            for (var i=0; i <= 9; i++)
+            {
+                var random = Math.floor(Math.random() * ($scope.data.length - 1));
+                $scope.stories[i] = $scope.data[random];
+            }
+
+            PopulateStories($scope.stories);
+
+
+        }, errorCallback);
+
+        function PopulateStories (storyIDArray)
+        {
+            angular.forEach(storyIDArray, function (storyID, key)
+            {
+
+                $http.get('https://hacker-news.firebaseio.com/v0/item/' + storyID + '.json').
+                    then(function successCallback2(response) {
+
+                        $scope.storyData[key] = response.data;
+
+                    }, errorCallback);
+
+                // $http.get('https://hacker-news.firebaseio.com/v0/user/' + $scope.storyData.by + '.json').
+                //     then(function (){
+                //         //$scope.storyData[key].authorKarma = response.data;
+                //         console.log(response.data);
+                //     }, errorCallback);
+                
+            });
+        }
+
+
+
+        var errorCallback = function (response) 
+        { 
+            console.log("ERROR:" + response.ExceptionMessage); 
+        }
 
 }]);
